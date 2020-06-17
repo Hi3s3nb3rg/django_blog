a59ddab42ab0b6d8ff7d9bea18b4fab108d6750e
Kyenshak David <62503079+Hi3s3nb3rg@users.noreply.github.com>
2020-06-17 09:02:57 +0100

    update settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -152,7 +152,7 @@ AWS_S3_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+#in case you recieved an authentication error from your amazon s3bucket
 AWS_S3_REGION_NAME = 'us-east-2' #change to your region
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
