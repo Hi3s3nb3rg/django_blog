da77d85fe9e94987e6f0c4cf68d691e27a491a88
Kyenshak David <62503079+Hi3s3nb3rg@users.noreply.github.com>
2020-06-17 09:04:55 +0100

    update forms

--- users/forms.py
+++ users/forms.py
@@ -12,7 +12,7 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-
+#using your model.py fields
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
 
